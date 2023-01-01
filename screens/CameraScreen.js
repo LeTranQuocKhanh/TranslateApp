ef6533d4d4dcb03782e8f986cf084001402c608d
LeTranQuocKhanh <20520574@gm.uit.edu.vn>
2023-01-01 16:36:28 +0700

    fix bug camare - untranslate image

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -4,12 +4,16 @@ import { useEffect, useRef, useState } from 'react';
 import { Camera } from 'expo-camera';
 import { shareAsync } from 'expo-sharing';
 import * as MediaLibrary from 'expo-media-library';
+import { useIsFocused } from '@react-navigation/native';
+import TextRecognition from 'react-native-text-recognition';
 
 export default function CameraScreen() {
   let cameraRef = useRef();
+  const isFocused = useIsFocused();
   const [hasCameraPermission, setHasCameraPermission] = useState();
   const [hasMediaLibraryPermission, setHasMediaLibraryPermission] = useState();
   const [photo, setPhoto] = useState();
+  const [text, setText] = useState("");
 
   useEffect(() => {
     (async () => {
@@ -18,6 +22,10 @@ export default function CameraScreen() {
       setHasCameraPermission(cameraPermission.status === "granted");
       setHasMediaLibraryPermission(mediaLibraryPermission.status === "granted");
     })();
+
+    // if(photo){
+
+    // }
   }, []);
 
   if (hasCameraPermission === undefined) {
@@ -35,6 +43,7 @@ export default function CameraScreen() {
 
     let newPhoto = await cameraRef.current.takePictureAsync(options);
     setPhoto(newPhoto);
+    console.log(newPhoto.uri)
   };
 
   if (photo) {
@@ -50,23 +59,38 @@ export default function CameraScreen() {
       });
     };
 
+
+    const convertToText = async () => {
+      console.log(photo.uri)
+      const result = await TextRecognition.recognize(photo.uri) ;
+      console.log('This is result', result)
+      setText(result)
+      console.log('This is text: ', text)
+    }
+
+
+
+
     return (
       <SafeAreaView style={styles.container}>
-        <Image style={styles.preview} source={{ uri: "data:image/jpg;base64," + photo.base64 }} />
+        {/* <Image style={styles.preview} source={{ uri: "data:image/jpg;base64," + photo.base64 }} /> */}
+        <Image style={styles.preview} source={{ uri: photo.uri }} />
         <Button title="Share" onPress={sharePic} />
         {hasMediaLibraryPermission ? <Button title="Save" onPress={savePhoto} /> : undefined}
         <Button title="Discard" onPress={() => setPhoto(undefined)} />
+        <Button title="Translate" onPress={convertToText} />
       </SafeAreaView>
     );
   }
 
   return (
-    <Camera style={styles.container} ref={cameraRef}>
-      <View style={styles.buttonContainer}>
-        <Button title="Take Pic" onPress={takePic} />
-      </View>
-      <StatusBar style="auto" />
-    </Camera>
+    isFocused && <Camera style={styles.container} ref={cameraRef}>
+    <View style={styles.buttonContainer}>
+      <Button title="Take Pic" onPress={takePic} />
+    </View>
+    <StatusBar style="auto" />
+  </Camera> 
+    
   );
 }
 
